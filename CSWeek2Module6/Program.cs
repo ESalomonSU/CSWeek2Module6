968b4414b24c6baed7b0b8067e321896e60eb4d6
Esteban Salomon <esalomon6492@southwestuniversity.edu>
2021-05-22 22:37:27 -0600

    CS Week 2 Module 6

---
+++ CSWeek2Module6/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace CSWeek2Module6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool menuOptions = true;
+            while (menuOptions)
+            {
+                menuOptions = Menu(); 
+            }
+
+            Console.ReadKey();
+             
+        }
+
+        private static bool Menu()
+        {
+            Console.Write("Select Dish:");
+            Console.Write("1. Meat");
+            Console.Write("2. Chicken");
+            Console.Write("3. Fish");
+
+            string option = Console.ReadLine();
+
+            if (option=="1")
+            {
+                Console.WriteLine("$5.99");
+
+                return true;
+            }
+            else if (option=="2")
+            {
+                Console.WriteLine("$4.99");
+
+                return true;
+            }
+            else if (option=="3")
+            {
+                Console.WriteLine("3.99");
+
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
